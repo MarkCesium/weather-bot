8cc24f556a92c045805c708423406f93493ff37e
MarkCesium <cesiummark01@gmail.com>
2024-06-26 00:32:36 +0300

    English local

--- handlers/start.py
+++ handlers/start.py
@@ -3,16 +3,14 @@ from aiogram.filters import Command
 from aiogram.types import Message, KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-start_router: Router = Router()
+router: Router = Router()
 
 
-@start_router.message(Command('start'))
+@router.message(Command("start"))
 async def start(message: Message):
     builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
-    builder.row(
-        KeyboardButton(text="Бягучая лакацацыя", request_location=True)
-    )
+    builder.row(KeyboardButton(text="Current location", request_location=True))
     await message.answer(
-        text='Вітанкі! Гэта Надвор\'е бот. Напішы назву горада ці скарыстайся кнопкай',
-        reply_markup=builder.as_markup(resize_keyboard=True)
+        text="Hi! It's the Weather Bot. Type in the name of the city or use the button.",
+        reply_markup=builder.as_markup(resize_keyboard=True),
     )
